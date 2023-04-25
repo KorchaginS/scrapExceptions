dfda671407db90c5fd7b2b04b4ca457062b90d45
KorchaginS <KorchaginSV1995@yandex.ru>
2023-04-25 21:20:09 +0300

    1`2

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-2
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
+    }
+
+def test_request(url, retry=5):
+
+    try:
+        response = requests.get(url=url, headers=headers)
+        print(f'[+] {url} {response.status_code}')
+    except Exception as ex:
+        if retry:
+            print(f'[INFO] retry={retry} => {url}')
+            return test_request(url, retry=(retry - 1))
+        else:
+            raise
+    else: return response
