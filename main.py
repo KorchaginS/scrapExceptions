2e5249b9b887de966368d66155852d63f29c3e33
KorchaginS <KorchaginSV1995@yandex.ru>
2023-04-25 22:08:14 +0300

    Допрошёл урок

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -14,8 +15,23 @@ def test_request(url, retry=5):
     except Exception as ex:
         if retry:
             print(f'[INFO] retry={retry} => {url}')
+            time.sleep(10)
             return test_request(url, retry=(retry - 1))
 
         else:
             raise
     else: return response
+
+def main():
+    with open('beooks.txt', 'r', encoding='utf-8') as file:
+        books_urls = file.read().splitlines()
+
+    for book_url in books_urls:
+        try:
+            r = test_request(url=book_url)
+            soup = BeautifulSoup(r.text, 'lxml')
+            print(f'{soup.title.text}\n{"-"*20}')
+        except:
+            continue
+if __name__ =='__main__':
+    main()
